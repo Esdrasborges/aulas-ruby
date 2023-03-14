0bcd129f8aa65a72185bcd304fbe819977724f9a
Esdrasborges <105466934+Esdrasborges@users.noreply.github.com>
2023-03-13 23:54:23 -0300

    deixae registrado

--- exer2.rb
+++ exer2.rb
@@ -5,7 +5,7 @@ array.each do |new_array|
 end
 
 #Outro Método Array#
-#esse método deve-se tirar o do 
+#esse método deve-se tirar o "do"#
 array =  [10, 20, 30, 40, 50, 60]
 array.each {|new_array|
     puts new_array
