f5f1910d76db2a9fdb4ae1e2f47e2d4dd2e82e9f
Esdrasborges <105466934+Esdrasborges@users.noreply.github.com>
2023-04-05 06:46:27 -0300

    começando metodos

---
+++ metodos.rb
@@ -0,0 +1,21 @@
+def soma(x, y)
+    return x + y 
+end
+
+def mult(x, y)
+    return x * y 
+end
+
+def subt(x, y)
+    return x - y 
+end
+
+def div(x, y)
+    return x / y 
+end
+
+puts soma(3,2)
+puts mult(8,5)
+puts subt(9,3)
+puts div(85,5)
+
